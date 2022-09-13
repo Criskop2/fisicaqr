b2d3da31b182a96d31d2554eb209b7fdfa3d8a46
Criskop2 <113443146+Criskop2@users.noreply.github.com>
2022-09-13 07:55:22 -0500

    Add files via upload

---
+++ database/database.js
@@ -0,0 +1,2 @@
+const mongoose = require("mongoose");
+module.exports = mongoose.connect("mongodb+srv://cluster0-mjsvu.mongodb.net/", { useNewUrlParser: true, useUnifiedTopology: true, user: "CrisKop", pass: "Nosequeponer123!", dbName: 'fisica'})
